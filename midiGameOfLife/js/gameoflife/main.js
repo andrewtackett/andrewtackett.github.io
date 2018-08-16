639f4a7c76f480748be71c1738a2b6f99ddd34dd
Andrew Tackett <andrew.tackett@gmail.com>
2018-08-16 09:03:41 -0500

    make notes play across columns with spacing, display which column is playing, allow putting in notes on the fly

--- midiGameOfLife/js/gameoflife/main.js
+++ midiGameOfLife/js/gameoflife/main.js
@@ -1,6 +1,16 @@
 var soundOnClick = true;
 var logging = false;
 var timeout = null;
+var trackerTimeout = null;
+var timeBetweenColumns = 250; //Milliseconds
+var timeBetweenFullPlay = (88+1) * timeBetweenColumns; //88 Notes
+
+//Constants
+var firstGameRow = 1;
+var lastGameRow = 27;
+var firstNote = 21;
+var lastNote = 108;
+var rowSpacerNum = 1000;
 
 var initialize = function(){
   MIDI.loadPlugin({
@@ -26,15 +36,15 @@ var initialize = function(){
 var createUI = function(){
   console.log("creating tables");
   $("#tracker").append("<tr>");
-  for(var num=21; num<109;num++){
-    $("#tracker").append("<td id='" + num + "'></td>")
+  for(var num=firstNote; num<=lastNote;num++){
+    $("#tracker").append("<td id='" + num + "' width='20px' height='20px'></td>")
   }
   $("#tracker").append("</tr>");
 
-  for(var numRows=1;numRows<27;numRows++){
+  for(var numRows=firstGameRow;numRows<lastGameRow;numRows++){
     $("#squares").append("<tr>");
-    for(var num=21;num<109;num++){
-      var cellID = (num + numRows * 1000);
+    for(var num=firstNote;num<=lastNote;num++){
+      var cellID = (num + numRows * rowSpacerNum);
       $("#squares").append("<td id=" +  cellID + " onclick='cellClick(\""+  num + "\",\"" + cellID + "\")' width='20px' height='20px'></td>");
     }
     $("#squares").append("</tr>");
@@ -61,6 +71,7 @@ var playNote = function(note){
 }
 
 var toggleCellColor = function(cellID){
+  console.log("toggle cell color: " + cellID);
   var curColor = $("#" + cellID).css("background-color");
   if(curColor == "rgb(0, 0, 0)"){
     $("#" + cellID).css("background-color", "rgb(255, 255, 255)");
@@ -77,10 +88,10 @@ var incrementGameState = function(){
 
 var calcFutureGameState = function(){
   var futureGameState = {};
-  for(var numRows=1;numRows<27;numRows++){
-    for(var num=21;num<109;num++){
+  for(var numRows=firstGameRow;numRows<lastGameRow;numRows++){
+    for(var num=firstNote;num<=lastNote;num++){
       var futureCellState = false; //DEAD
-      var cellID = (num + numRows * 1000);
+      var cellID = (num + numRows * rowSpacerNum);
       var cellNeighbors = [cellID + 1, cellID -1, cellID + 1000, cellID - 1000, cellID + 1001, cellID - 1001, cellID + 999, cellID - 999];
       var numAliveNeighbors = checkNumAliveNeighbors(cellNeighbors);
       if(numAliveNeighbors < 2 || numAliveNeighbors > 3){
@@ -104,7 +115,7 @@ var checkNumAliveNeighbors = function(cellNeighbors){
   for(var index in cellNeighbors){
     var cellID = cellNeighbors[index];
     //Handle boundary conditions
-    if(cellID < 1000 || cellID > 27000 || cellID%1000 < 21 || cellID%1000 > 108){
+    if(cellID < (firstGameRow * rowSpacerNum) || cellID > (lastGameRow * rowSpacerNum) || cellID%rowSpacerNum < firstNote || cellID%rowSpacerNum > lastNote){
       continue;
     }else{
       if(cellAlive(cellID)){
@@ -122,35 +133,59 @@ var cellAlive = function(cellID){
 
 var applyFutureGameState = function(futureGameState){
   console.log("applyFutureGameState");
-  var notesToPlay = new Set();
   for(var cellID in futureGameState){
     if(futureGameState[cellID]){
       reviveCell(cellID);
-      notesToPlay.add(cellID%1000);
     }else{
       killCell(cellID);
     }
   }
 
-  console.log("playing notes?");
-  notesToPlay.forEach(function callback(value1, value2, Set) {
-    console.log("play note: " + value1);
-    playNote(value1);
-  })
+  playMIDINotes(firstNote); //Start point is first note
 }
 
 var killCell = function(cellID){
-  $("#" + cellID).css("background-color","rgb(255,255,255)");
+  $("#" + cellID).css("background-color","rgb(255, 255, 255)");
 }
 
 var reviveCell = function(cellID){
-  $("#" + cellID).css("background-color","rgb(0,0,0)");
+  $("#" + cellID).css("background-color","rgb(0, 0, 0)");
+}
+
+var playMIDINotes = function(curNum){
+  console.log("playing notes: " + curNum);
+  if(curNum > lastNote){
+    console.log("killing all tracker row");
+    for(var num=firstNote;num<=lastNote;num++){
+      $("#" + num).css('background-color',"rgb(255,255,255)");
+    }
+  }else{
+    $("#" + curNum).css('background-color',"rgb(0, 0, 0)");
+    if(checkIfColumnHasAlive(curNum)){
+      playNote(curNum);
+    }
+    trackerTimeout = setTimeout(function(){
+      playMIDINotes(curNum+1);
+    },timeBetweenColumns);
+  }
+}
+
+var checkIfColumnHasAlive = function(columnID){
+  for(var row=firstGameRow;row<lastGameRow;row++){
+    var cellID = columnID + row * rowSpacerNum;
+    if(cellAlive(cellID)){
+      return true;
+    }
+  }
+  return false;
 }
 
 var startGoL = function(){
+  console.log("start game");
+  incrementGameState();
   timeout = setInterval(function(){
     incrementGameState();
-  },1000);
+  },timeBetweenFullPlay);
 }
 
 var incrementGoL = function(){
@@ -158,15 +193,18 @@ var incrementGoL = function(){
 }
 
 var stopGoL = function(){
+
   clearInterval(timeout);
+  clearInterval(trackerTimeout);//TODO: reinitialize from there if restarted?
 }
 
 var resetGoL = function(){
+  console.log("resetting");
   stopGoL();
 
-  for(var numRows=1;numRows<27;numRows++){
-    for(var num=21;num<109;num++){
-      var cellID = (num + numRows * 1000);
+  for(var numRows=0;numRows<lastGameRow;numRows++){
+    for(var num=firstNote;num<=lastNote;num++){
+      var cellID = (num + numRows * rowSpacerNum);
       $("#" + cellID).css("background-color","rgb(255,255,255)");
     }
   }
